9bf9ec22e77ddcf634d2c9c207ee0948473b169c
Sergi Pineda <sergipineda@iesebre.com>
2015-10-29 13:11:41 +0100

    Inicialitacio del repositori

---
+++ src/caractaristiquesllenguatge/Tipatge.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caractaristiquesllenguatge;
+
+/**
+ *
+ * @author dispineda
+ */
+public class Tipatge {
+
+    //Tipateg estàtic
+    
+    int a =3;
+    
+    
+    public static void main(String[] args) {
+       Tipatge A = new Tipatge();
+       
+       A.a="fmewlfmñ";
+    }
+    
+}
